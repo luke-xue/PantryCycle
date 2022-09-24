7941f3a6c0087f6331121567743c1aa78dd949cf
Luke Xue <luqi9900@gmail.com>
2022-09-24 03:56:18 -0400

    restyling restaurant

--- components/Chooserpage.js
+++ components/Chooserpage.js
@@ -14,72 +14,78 @@ import {
   AspectRatio,
   Stack,
   Text,
-
 } from "native-base";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Restaurant = () => {
-  return <Box alignItems="center">
-      <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
-      borderColor: "coolGray.600",
-      backgroundColor: "gray.700"
-    }} _web={{
-      shadow: 2,
-      borderWidth: 0
-    }} _light={{
-      backgroundColor: "gray.50"
-    }}>
-        <Box>
-          <AspectRatio w="100%" ratio={16 / 9}>
-            <Image source={{
-            uri: "https://spicecravings.com/wp-content/uploads/2020/12/Chicken-Kathi-Roll-Featured-1.jpg"
-          }} alt="image" />
-          </AspectRatio>
-          <Center bg="violet.500" _dark={{
-          bg: "violet.400"
-        }} _text={{
-          color: "warmGray.50",
-          fontWeight: "700",
-          fontSize: "xs"
-        }} position="absolute" bottom="0" px="3" py="1.5">
-            PHOTOS
-          </Center>
-        </Box>
-        <Stack p="4" space={3}>
-          <Stack space={2}>
-            <Heading size="md" ml="-1">
-              Roti Roll
-            </Heading>
-            <Text fontSize="xs" _light={{
-            color: "violet.500"
-          }} _dark={{
-            color: "violet.400"
-          }} fontWeight="500" ml="-0.5" mt="-1">
-              MMMMMMMmmmm so yummy
-            </Text>
-          </Stack>
-          <Text fontWeight="400">
-            yummy Bombay Frankie
-          </Text>
-          <HStack alignItems="center" space={4} justifyContent="space-between">
-            <HStack alignItems="center">
-              <Text color="coolGray.600" _dark={{
-              color: "warmGray.200"
-            }} fontWeight="400">
-                6 mins ago
+  return (
+    <Box alignItems="center">
+      <Box
+        maxW="80"
+        rounded="lg"
+        overflow="hidden"
+        borderColor="coolGray.200"
+        borderWidth="1"
+        _dark={{
+          borderColor: "coolGray.600",
+          backgroundColor: "gray.700",
+        }}
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+        _light={{
+          backgroundColor: "gray.50",
+        }}
+      >
+        <Stack direction="row">
+          <Box>
+            <AspectRatio w="130" ratio={1 / 1}>
+              <Image
+                source={{
+                  uri: "https://spicecravings.com/wp-content/uploads/2020/12/Chicken-Kathi-Roll-Featured-1.jpg",
+                }}
+                alt="image"
+              />
+            </AspectRatio>
+          </Box>
+          <Stack p="4" space={3}>
+            <Stack space={2}>
+              <Heading size="md" ml="-1">
+                Roti Roll
+              </Heading>
+              <Text
+                fontSize="xs"
+                _light={{
+                  color: "violet.500",
+                }}
+                _dark={{
+                  color: "violet.400",
+                }}
+                fontWeight="500"
+                ml="-0.5"
+                mt="-1"
+              >
+                MMMMMMMmmmm so yummy
               </Text>
+            </Stack>
+            <Text fontWeight="400">yummy Bombay Frankie</Text>
+            <HStack
+              alignItems="center"
+              space={4}
+              justifyContent="space-between"
+            >
             </HStack>
-          </HStack>
+          </Stack>
         </Stack>
       </Box>
-    </Box>;
+    </Box>
+  );
 };
 
 const Chooserpage = ({ navigation }) => {
-  return(
-    <Restaurant/>
-  )
-}
+  return <Restaurant />;
+};
 
 export default Chooserpage;
