df1339a0a2a42d41a5400a42af7bf8737fb5ef97
rohan-r-kulkarni <rohan.kulkarni1023@gmail.com>
2022-09-23 22:52:30 -0400

    yaml

--- fire.py
+++ fire.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import db
 from firebase_admin import credentials
 import json
+import webapp2
 
 cred_object = credentials.Certificate("./divhacks-d18b7-firebase-adminsdk-odcaf-7a5ab53b14.json")
 default_app = firebase_admin.initialize_app(cred_object, {
@@ -31,3 +32,14 @@ with open("books.json", "r") as f:
 
 for key, value in file_contents.items():
 	ref.push().set(value)
+
+##gcloud
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
