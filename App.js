5e0f15d4d378cb019d0b851b93778bbea0f1f6d8
elise <meike.yang@gmail.com>
2022-09-24 08:28:23 -0400

    merge

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NativeBaseProvider} from "native-base";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Homepage from "./components/Homepage.js";
+// import Giverpage from "./components/Giverpage.js";
+import Giverpage2 from "./components/Giverpage2.js";
 import Chooserpage from "./components/Chooserpage.js";
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +16,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Homepage} />
+          <Stack.Screen name="Giver" component={Giverpage2} />
           <Stack.Screen name="Chooser" component={Chooserpage} />
         </Stack.Navigator>
       </NavigationContainer>
