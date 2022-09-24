23fba6605e79b24ba68579eb644eca7230fd0c21
Luke Xue <luqi9900@gmail.com>
2022-09-24 03:35:08 -0400

    added chooser page

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NativeBaseProvider, Box, HStack, VStack, Pressable, Image, Center, Heading, FormControl, Input, Link, Button } from "native-base";
+import { NativeBaseProvider} from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Homepage from "./components/Homepage.js";
+import Chooserpage from "./components/Chooserpage.js";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Homepage/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Homepage} />
+          <Stack.Screen name="Chooser" component={Chooserpage} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
--- components/Homepage.js
+++ components/Homepage.js
@@ -1,17 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
-import { NativeBaseProvider, Box, HStack, VStack, Pressable, Image, Center, Heading, FormControl, Input, Link, Button } from "native-base";
+import {
+  NativeBaseProvider,
+  Box,
+  HStack,
+  VStack,
+  Pressable,
+  Image,
+  Center,
+  Heading,
+  FormControl,
+  Input,
+  Link,
+  Button,
+} from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Homepage = () => {
-  return <Center w="100%">
+const Homepage = ({ navigation }) => {
+  return (
+    <Center w="100%">
       <Box safeArea p="2" py="8" w="90%" maxW="290">
-        <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
-        color: "warmGray.50"
-      }}>
-          Welcome to beggars can't be choosers
+        <Heading
+          size="lg"
+          fontWeight="600"
+          color="coolGray.800"
+          _dark={{
+            color: "warmGray.50",
+          }}
+        >
+          Welcome to PantryCycle
         </Heading>
-        <Heading mt="1" _dark={{
-        color: "warmGray.200"
-      }} color="coolGray.600" fontWeight="medium" size="xs">
+        <Heading
+          mt="1"
+          _dark={{
+            color: "warmGray.200",
+          }}
+          color="coolGray.600"
+          fontWeight="medium"
+          size="xs"
+        >
           Please select your role.
         </Heading>
 
@@ -19,12 +46,16 @@ const Homepage = () => {
           <Button mt="2" colorScheme="indigo">
             Beggar
           </Button>
-          <Button mt="2" colorScheme="indigo">
+          <Button mt="2" colorScheme="indigo"
+          onPress={() =>
+            navigation.navigate('Chooser')
+          }>
             Chooser
           </Button>
         </VStack>
       </Box>
-    </Center>;
+    </Center>
+  );
 };
 
-export default Homepage
+export default Homepage;
