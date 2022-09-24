018a4192eeb1b1fa991c2f05a9f6fe6891a05c14
therealrnager <rkuang8@gmail.com>
2022-09-24 05:21:48 -0400

    app and fire

--- server/fire.py
+++ server/fire.py
@@ -3,29 +3,20 @@ from firebase_admin import db
 from firebase_admin import credentials
 import json
 
-cred_object = credentials.Certificate("./divhacks-d18b7-firebase-adminsdk-odcaf-7a5ab53b14.json")
-default_app = firebase_admin.initialize_app(cred_object, {
-	'databaseURL':"https://divhacks-d18b7-default-rtdb.firebaseio.com/"
+def initialize():
+	cred_object = credentials.Certificate("./divhacks-d18b7-firebase-adminsdk-odcaf-7a5ab53b14.json")
+	default_app = firebase_admin.initialize_app(cred_object, {
+		'databaseURL':"https://divhacks-d18b7-default-rtdb.firebaseio.com/"
+		})
+
+def reset():
+	# print("0")
+	ref = db.reference("/")
+	# print("1")
+	ref.set({
+		"All" : {}
 	})
-
-ref = db.reference("/")
-
-ref = db.reference("/")
-# ref.set({
-# 	"PlateApp":
-# 	{
-# 		"Carbs": -1,
-# 		"Veggies":-1,
-# 		"Fruits":-1,
-# 		"Protein":-1,
-# 		"Dairy":-1
-# 	}
-# })
-
-# with open("books.json", "r") as f:
-# 	file_contents = json.load(f)
-
-food = {"Name":"Burger","Type":"Protein", "Lat":1, "Long":2, "Footprint":1}
+	# print("2")
 
 # for key, value in file_contents.items():
 # 	ref.push().set(value)
@@ -33,11 +24,28 @@ food = {"Name":"Burger","Type":"Protein", "Lat":1, "Long":2, "Footprint":1}
 # 	ref = db.reference("/PlateApp/Protein")
 # 	ref.push().set(food)
 
-def writedb(json):
-	if food["Type"] == "Protein":
-		ref = db.reference("/PlateApp/Protein")
-		ref.push().set(food)
-	#veggie, fruit
+def writedb(Restaurant, Name, Type, Count, Lat, Long, Calories, Footprint):
+	food = {"Restaurant": Restaurant, "Name":Name,"Type":Type, "Count": Count,"Lat":Lat, "Long":Long, "Calories": Calories, "Footprint": Footprint}
 
-def readdb(json):
-	print(ref.get())
+	directory = "/All/"
+
+	ref = db.reference(directory)
+	ref.child(Restaurant).set(food)
+
+	# ref.push().set(food)
+	#veggie, fruit
+	print("yay")
+
+def readAll():
+	directory = "/All/" 
+	ref = db.reference(directory)
+	return ref.get()
+
+
+def rank():
+	directory = "/All/" 
+	ref = db.reference(directory)
+	all_shops = ref.get()[directory]
+	for shop in all_shops:
+		calories = shop["Calories"]
+
